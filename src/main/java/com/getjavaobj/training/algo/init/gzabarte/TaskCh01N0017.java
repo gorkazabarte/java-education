236a59c1e3ae1b04b7ea90311d721f7d83e98b11
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2023-05-15 12:29:36 +0200

    TaskCh01N017
    Refractor code:
    1. Add appropriate string documenting the functions.
    2. Remove useless blank lines

--- src/main/java/com/getjavaobj/training/algo/init/gzabarte/TaskCh01N0017.java
+++ src/main/java/com/getjavaobj/training/algo/init/gzabarte/TaskCh01N0017.java
@@ -1,47 +1,56 @@
-package main.java.com.getjavajob.training.algo.init.gzabarte;
+package com.getjavaobj.training.algo.init.gzabarte;
 
 import java.lang.Math;
 
 public class TaskCh01N0017 {
-
-    public static double sin(double x) {
-        /*
-        Calculate square root of 1 - sin²(x).
-        */
-
+    /**
+     * Calculate square root of 1 - sin²(x)
+     *
+     * @param x double number
+     * @return result of the operation
+     */
+    public static double operation1(double x) {
         return Math.sqrt(1.0 - (Math.sin(x) * Math.sin(x)));
     }
 
-    public static double operation1(double a, double b, double c, double x) {
-        /*
-        Calculate [1] / [sqrt(ax² + bx + c)]
-        */
-
+    /**
+     * Calculate [1] / [sqrt(ax² + bx + c)]
+     *
+     * @param a double parameter
+     * @param b double parameter
+     * @param c double parameter
+     * @param x double value
+     * @return result of the operation
+     */
+    public static double operation2(double a, double b, double c, double x) {
         return 1 / (Math.sqrt((a * Math.pow(x, 2.0)) + (b * x) + c));
     }
 
-    public static double operation2(double x) {
-        /*
-        Calculate [sqrt(x + 1) - sqrt(x - 1)] / [2 * sqrt(x)]
-        */
-
+    /**
+     * Calculate [sqrt(x + 1) - sqrt(x - 1)] / [2 * sqrt(x)]
+     *
+     * @param x double number
+     * @return result of the operation
+     */
+    public static double operation3(double x) {
         return (Math.sqrt(x + 1) - Math.sqrt(x - 1)) / (2 * Math.sqrt(x));
     }
 
-    public static double abs_value(double x) {
-        /*
-        Calculate |x| + |x + 1|
-        */
-
+    /**
+     * Calculate |x| + |x + 1|
+     *
+     * @param x double number
+     * @return result of the operation
+     */
+    public static double operation4(double x) {
         return Math.abs(x) + Math.abs(x + 1.0);
-
     }
 
     public static void main(String[] args) {
 
-        System.out.println(sin(10.0));
-        System.out.println(operation1(1.0, 1.0, 1.0, 1.0));
-        System.out.println(operation2(10.0));
-        System.out.println(abs_value(10.0) + " != " + abs_value(-10.0));
+        System.out.println(operation1(10.0));
+        System.out.println(operation2(1.0, 1.0, 1.0, 1.0));
+        System.out.println(operation3(10.0));
+        System.out.println(operation4(10.0) + " != " + operation4(-10.0));
     }
 }
