9c30cc78b9caaab13bd10a3a14a154fec6ab9327
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2023-05-19 01:00:15 +0200

    TaskCh02N013
    Refractor code in TestClass

--- src/main/java/com/getjavaobj/training/algo/init/gzabarte/TaskCh02N013Test.java
+++ src/main/java/com/getjavaobj/training/algo/init/gzabarte/TaskCh02N013Test.java
@@ -1,17 +1,16 @@
 package com.getjavaobj.training.algo.init.gzabarte;
 
-import com.getjavaobj.training.algo.init.gzabarte.TaskCh02N0013;
 import com.getjavaobj.training.algo.util.Assert;
 
-public class TaskCh02N0013Test {
+public class TaskCh02N013Test {
     public static void testEvenNumber() {
-        TaskCh02N0013 o = new TaskCh02N0013();
+        TaskCh02N013 o = new TaskCh02N013();
         Assert.assertEquals("TaskCh02N001.reverseNumber", 543, o.reverseNumber(345));
         Assert.assertEquals("TaskCh02N001.reverseNumber", 1, o.reverseNumber(100));
     }
 
     public static void testOddNumber() {
-        TaskCh02N0013 o = new TaskCh02N0013();
+        TaskCh02N013 o = new TaskCh02N013();
         Assert.assertEquals("TaskCh02N001.reverseNumber", o.reverseNumber(123), 321);
     }
 
