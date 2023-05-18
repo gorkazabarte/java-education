68b23dce704e0cef58b9081b3b8d70b934a0e998
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2023-05-19 00:42:20 +0200

    TaskCh01N017
    Refractor code (write clean code using Java good practices)

--- src/main/java/com/getjavaobj/training/algo/init/gzabarte/TaskCh01N0017.java
+++ src/main/java/com/getjavaobj/training/algo/init/gzabarte/TaskCh01N0017.java
@@ -10,7 +10,7 @@ public class TaskCh01N0017 {
      * @return result of the operation
      */
     public static double operation1(double x) {
-        return Math.sqrt(1.0 - (Math.sin(x) * Math.sin(x)));
+        return Math.sqrt(1.0 - (Math.pow(Math.sin(x), 2)));
     }
 
     /**
@@ -19,21 +19,21 @@ public class TaskCh01N0017 {
      * @param a double parameter
      * @param b double parameter
      * @param c double parameter
-     * @param x double value
+     * @param x double number
      * @return result of the operation
      */
     public static double operation2(double a, double b, double c, double x) {
-        return 1 / (Math.sqrt((a * Math.pow(x, 2.0)) + (b * x) + c));
+        return 1 / Math.sqrt(a * Math.pow(x, 2.0) + b * x + c);
     }
 
     /**
-     * Calculate [sqrt(x + 1) - sqrt(x - 1)] / [2 * sqrt(x)]
+     * Calculate [sqrt(x + 1) + sqrt(x - 1)] / [2 * sqrt(x)]
      *
      * @param x double number
      * @return result of the operation
      */
     public static double operation3(double x) {
-        return (Math.sqrt(x + 1) - Math.sqrt(x - 1)) / (2 * Math.sqrt(x));
+        return (Math.sqrt(x + 1) + Math.sqrt(x - 1)) / 2 * Math.sqrt(x);
     }
 
     /**
@@ -47,7 +47,6 @@ public class TaskCh01N0017 {
     }
 
     public static void main(String[] args) {
-
         System.out.println(operation1(10.0));
         System.out.println(operation2(1.0, 1.0, 1.0, 1.0));
         System.out.println(operation3(10.0));
