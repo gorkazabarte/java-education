255a811990b9dcc8609a6cd8f353d9f0b78dee83
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2023-05-22 17:45:05 +0200

    Simple program explaining variable basics

---
+++ src/main/java/com.getjavaobj.training.algo.init.gzabarte/introduction/Example2.java
@@ -0,0 +1,13 @@
+package com.getjavaobj.training.algo.init.gzabarte.introduction;
+
+/** This class demonstrates a variable */
+public class Example2 {
+    public static void main(String [] args) {
+        int var1;
+        int var2;
+        var1 = 1024;
+        System.out.printf("Var1 contains %d\n", var1);
+        var2 = var1/2;
+        System.out.printf("Var2 contains %d\n", var2);
+    }
+}
