d92aa3bb6b943f494098193adb809563f5d5c851
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2023-05-22 17:44:29 +0200

    Java Hello world

---
+++ src/main/java/com.getjavaobj.training.algo.init.gzabarte/introduction/Example.java
@@ -0,0 +1,10 @@
+package com.getjavaobj.training.algo.init.gzabarte.introduction;
+
+/**
+ * This is a single Example.java
+ */
+public class Example {
+    public static void main(String [] args) {
+        System.out.printf("Java drives the web by %s", args[0]);
+    }
+}
